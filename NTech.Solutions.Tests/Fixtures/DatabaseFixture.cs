60b0aa181595299f06e5576c5ff71a8ae79628f7
Nikolaj Have <nhave06@gmail.com>
2026-02-17 14:05:45 +0100

    Organized UnitTests

---
+++ NTech.Solutions.Tests/Fixtures/DatabaseFixture.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.PostgreSql;
+
+namespace NTech.Solutions.Tests.Fixtures
+{
+    public class DatabaseFixture : IAsyncLifetime
+    {
+        public IServiceProvider Services;
+        //private PostgreSqlContainer _container;
+
+        public DatabaseFixture()
+        {
+            //_container = new PostgreSqlBuilder("postgres")
+            //    .Build();
+
+            var coll = new ServiceCollection();
+
+            //coll.AddTransient<IConfiguration>(_ =>
+            //{
+            //    var configs = new List<KeyValuePair<string, string?>>
+            //    {
+            //        new ("ConnectionStrings:PostgresDb", _container.GetConnectionString())
+            //    };
+
+            //    return new ConfigurationBuilder()
+            //        .AddInMemoryCollection(configs)
+            //        .Build();
+            //});
+
+            //coll.AddDbContext<AppDbContext>();
+
+            Services = coll.BuildServiceProvider();
+        }
+
+        public async Task InitializeAsync()
+        {
+            //await _container.StartAsync();
+            //var context = Services.GetRequiredService<AppDbContext>();
+            //await context.Database.EnsureCreatedAsync();
+        }
+
+        public async Task DisposeAsync()
+        {
+            //await _container.StopAsync();
+        }
+    }
+}
---
+++ NTech.Solutions.Tests/UnitTests/DatabaseTests.cs
@@ -0,0 +1,13 @@
+﻿using NTech.Solutions.Tests.Fixtures;
+
+namespace NTech.Solutions.Tests.UnitTests
+{
+    public class DatabaseTests(DatabaseFixture db) : IClassFixture<DatabaseFixture>
+    {
+        [Fact]
+        public async Task UserRegistry()
+        {
+
+        }
+    }
+}
